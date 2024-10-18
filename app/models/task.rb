9b2473869b7a3167d6219813c9880f164ce48122
markatpari@gmail.com <markatpari@gmail.com>
2024-10-19 05:52:18 +0700

    validate table, Add relative, Add feature

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
   enum status: { ongoing: 0, complete: 1 }
-
   validates :status, presence: true, inclusion: { in: statuses.keys }
+  validates :catego, presence: true
+  belongs_to :catego
+  def toggle_status
+    self.status = ongoing? ? :complete : :ongoing
+    save
+  end
 end
